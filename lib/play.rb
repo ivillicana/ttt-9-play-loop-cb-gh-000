0e1201a17f2fdce86e3f4714d079ec0ae4c3b6a3
Isaac Villicana <villicana.i@gmail.com>
2018-03-20 01:38:35 +0000

    automatically backed up by learn

--- lib/play.rb
+++ lib/play.rb
@@ -13,10 +13,10 @@ end
 
 def move(board, index, current_player = "X")
   board[index] = current_player
-  if current_player == 'X'
-    current_player = 'O'
+  if current_player == "X"
+    current_player = "O"
   else
-    current_player = 'X'
+    current_player = "X"
   end
 end
 
